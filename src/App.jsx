c789902852866b303a011b6f8a0014e1d894e7a2
amlcheung <cheung.as@northeastern.edu>
2022-04-01 14:01:53 -0700

    original files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+    <body>
+      <div>
+          <div class="header-text header-box">Welcome to Wordle</div>
+
+          <div class="navbar-container">
+          <a href="">Instructions</a>
+          <a href="https://amlcheung.github.io/project1/Projects/">Easy</a>
+          <a href="https://amlcheung.github.io/project1/AboutMe/">Medium</a>
+          <a href="https://amlcheung.github.io/project1/Contacts/">Hard</a>
+        </div>
+      </div>
+    </body>
+    
+  );
+}
+
+export default App;
