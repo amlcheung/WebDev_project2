2b7cdfd62e79043aff6c220ad33aeadd888edce5
mvanzuylen <madeleine.vanzuylen@gmail.com>
2022-04-11 14:53:52 -0700

    fixed message baord

--- src/component/MessageBoard.jsx
+++ src/component/MessageBoard.jsx
@@ -7,36 +7,50 @@ export default function MessageBoard() {
     
 
     function createMessage(state) {
-        let message = "";
-
-        if (state.invalidWordLength) {
+       let message = "";
+        
+        if (state.current_diff == "EASY" && state.easyinvalidWordLength == true){
             message = "Invalid Word Length";
         }
-        if (state.invalidWord){
+        else if (state.current_diff == "MEDIUM" && state.medinvalidWordLength == true){
+            message = "Invalid Word Length";
+        }
+        else if (state.current_diff == "HARD" && state.hardinvalidWordLength == true){
+             message = "Invalid Word Length";
+        } 
+    
+        if (state.current_diff == "EASY" && state.easyinvalidWord == true){
             message = "Word not in dictionary";
         }
+            else if (state.current_diff == "MEDIUM" && state.medinvalidWord == true){
+                message = "Word not in dictionary";
+        }
+        else if (state.current_diff == "HARD" && state.hardinvalidWord == true){
+            message = "Word not in dictionary";
+        } 
+    
         let easycurGuess = state.easy_history[state.easy_history.length - 1];
         let medcurGuess = state.med_history[state.med_history.length - 1];
         let hardcurGuess = state.hard_history[state.hard_history.length - 1];
 
         if (easycurGuess !== undefined) {
-            if (state.easy_word === easycurGuess.toUpperCase()) {
+            if (state.current_diff == "EASY" && state.easy_word === easycurGuess.toUpperCase()) {
                 message = "Congratulations!  Would you like to try again?";
-            } else if (state.easyattemptsRemaining === 0 && state.easy_word !== easycurGuess.toUpperCase()) {
+            } else if (state.current_diff == "EASY" && state.easyattemptsRemaining === 0 && state.easy_word !== easycurGuess.toUpperCase()) {
                 message = "You failed to guess the word. End of Game.";
             }
         }
         if (medcurGuess !== undefined) {
-            if (state.med_word === medcurGuess.toUpperCase()) {
+            if (state.current_diff == "MEDIUM" && state.med_word === medcurGuess.toUpperCase()) {
                 message = "Congratulations!  Would you like to try again?";
-            } else if (state.medattemptsRemaining === 0 && state.med_word !== medcurGuess.toUpperCase()) {
+            } else if (state.current_diff == "MEDIUM" && state.medattemptsRemaining === 0 && state.med_word !== medcurGuess.toUpperCase()) {
                 message = "You failed to guess the word. End of Game.";
             }
         }
         if (hardcurGuess !== undefined) {
-            if (state.hard_word === hardcurGuess.toUpperCase()) {
+            if (state.current_diff == "HARD" && state.hard_word === hardcurGuess.toUpperCase()) {
                 message = "Congratulations!  Would you like to try again?";
-            } else if (state.hardattemptsRemaining === 0 && state.hard_word !== easycurGuess.toUpperCase()) {
+            } else if (state.current_diff == "HARD" && state.hardattemptsRemaining === 0 && state.hard_word !== easycurGuess.toUpperCase()) {
                 message = "You failed to guess the word. End of Game.";
             }
         }
